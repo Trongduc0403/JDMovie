fd45dcd8c6787179283db425821924cf4cff48ac
Joyce <77377941+Joyce2709@users.noreply.github.com>
2021-12-16 22:38:50 +0700

    uppppp

--- JDMovie/Areas/Admin/Controllers/HomeController.cs
+++ JDMovie/Areas/Admin/Controllers/HomeController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JDMovie.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JDMovie.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class HomeController : Controller
     {
-        [Area("Admin")]
+        
+        private dbDACNContext db = new dbDACNContext();
         public IActionResult Index()
         {
+            var adminphimle = (from phimle in db.DsphimLes select phimle).ToList().Take(5);
+            var admintintuc = (from tintuc in db.Tintucphims select tintuc).ToList().Take(5);
+            var adminphimbo = (from phimbo in db.DsphimBos select phimbo).ToList().Take(5);
+            var admintaikhoan = (from taikhoan in db.TaiKhoans select taikhoan).ToList().Take(5);
+
+            var slphimle = (from phimlesl in db.DsphimLes select phimlesl).Count();
+            var sltintuc = (from tintucsl in db.Tintucphims select tintucsl).Count();
+            var slphimbo = (from phimbosl in db.DsphimBos select phimbosl).Count();
+            var sltaikhoan = (from taikhoansl in db.TaiKhoans select taikhoansl).Count();
+            ViewBag.phimle = adminphimle;
+            ViewBag.phimbo = adminphimbo;
+            ViewBag.tintuc = admintintuc;
+            ViewBag.taikhoan = admintaikhoan;
+
+
+            ViewBag.phimlesl = slphimle;
+            ViewBag.phimbosl = slphimbo;
+            ViewBag.tintucsl = sltintuc;
+            ViewBag.taikhoansl = sltaikhoan;
             return View();
         }
     }
